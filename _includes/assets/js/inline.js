39e94f396eecb9ca94404745a34804cdd1783b37
acem702 <55085012+acem702@users.noreply.github.com>
2023-08-20 15:45:15 +0100

    update _includes/assets/js/inline.js

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -1,1 +1,18 @@
-// Add your inline JS here
+// Add your inline JS 
+/*Confignav*************************************/
+function showConfig() {
+  if (offen.wert == "nein") {
+    document.getElementById("myconfig").style.height = "100vh";
+    document.getElementById("myconfig").style.width = "100%";
+    offen.wert = "ja";
+    hideFab();
+  } else {
+    closeConfig();
+    hideFab();
+  }
+}
+function closeConfig() {
+  offen.wert = "nein";
+  document.getElementById("myconfig").style.height = "0px";
+  document.getElementById("myconfig").style.width = "0px";
+}
